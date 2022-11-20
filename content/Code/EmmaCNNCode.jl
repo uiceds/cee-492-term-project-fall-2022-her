57b2d78878858803d77dbfedac47062fb9f1ef2e
emmagolub <emmaa.golub@gmail.com>
2022-11-20 11:56:46 -0800

    CodeUpdates, see CNNCode

---
+++ content/Code/EmmaCNNCode.jl
@@ -0,0 +1,114 @@
+import Pkg; 
+Pkg.add("ProgressLogging")
+Pkg.add("MLUtils")
+Pkg.add("LinearAlgebra")
+
+using PlutoUI
+using CSV
+using DataFrames
+using Plots
+using Statistics
+using LinearAlgebra
+using ProgressLogging
+using Flux
+using MLUtils
+
+
+x = let
+	x0 = CSV.read("C:/Users/emmag/OneDrive/EMMA_user/Docs/Academics/UIUC/2022-2023/CEE 492 Data Science/Project/TreeData/Data/TS3_Raw_tree_data.csv", DataFrame)
+    x1 = select(x0,["CommonName", "Age", "DBH", "TreeHt", "CrnBase", "CrnHt", "CdiaPar", "CDiaPerp", "AvgCdia", "Leaf"])
+    x2 = filter(x1 -> (x1.Age != -1) && (x1.DBH != -1) && (x1.TreeHt != -1) && (x1.CrnBase != -1) && (x1.CrnHt != -1) && (x1.CdiaPar != -1) && (x1.CDiaPerp != -1) && (x1.AvgCdia != -1) && (x1.Leaf != -1), x1)
+    x3 = select(x2, Not(["CommonName"]))
+    x4 = transpose(Matrix(x3))
+    reshape(x4, size(x4, 1), 1, size(x4,2))
+end
+println(x)
+
+y = let 
+    y1 = select(x2,["CommonName"])
+    #println(y1)
+    y2 = unique(y1)
+    #println(y2)
+
+
+
+   
+    #namesToID = Dict(pairs(y2.CommonName))
+    #idToNames = Dict(v => k for (k, v) in namesToID) 
+    #y_test = (namesToID[name] for name in y1) #not working, trying to write each species name in the original dataframe of species y1 to an ID
+    #d = Dict(pairs(eachrow(y_test))) 
+    #yID = collect(keys(d)) # d needs to be a dictionary, trying to create one from y_test data frame, but Base.Generator data frame type is prohibiting.
+
+    y1 = transpose((y1))
+	Flux.onehotbatch(y1, 0:9)
+end
+
+let
+	plots = []
+	for i in 1:16
+		# We can convert the one-hot labels back into the category numbers using the 
+		# 'onecold' command.
+		p = plot(x[:, 1, i],1:size(x,1), title=Flux.onecold(y[:, i])-1, 
+			label=:none, titlefontsize=9)
+		push!(plots, p)
+	end
+	plot(plots..., size=(650, 650))
+end
+
+# Neural Network Model
+model = Chain(
+   	Conv((5,), 1 => 5, pad=(1,), relu),
+	Conv((3,), 5 => 7, pad=(1,), relu),
+	Conv((7,), 7 => 15, pad=(1,), relu), 
+	Conv((5,), 15 => 7, pad=(1,), relu),
+	Conv((3,), 7 => 5, pad=(1,), relu),
+	x -> reshape(x, :, size(x,3)),
+	#x -> maxpool(x, (2,2)),
+	Dense(160, 80),
+	Dense(80, 10),
+	softmax,
+)
+
+accuracy(ŷ, y) = mean(Flux.onecold(ŷ) .== Flux.onecold(y))
+# Accuracy before training
+accuracy(model(x), y)
+
+function loss(x, y) 
+	Flux.Losses.crossentropy(model(x),y)
+end
+
+
+begin
+	batchsize = 100
+	epochs = 125
+	η = 0.001
+end;
+
+begin
+	data = Flux.DataLoader((x, y), batchsize=batchsize);
+	loss_hist=zeros(epochs)
+	accuracy_hist=zeros(epochs)
+	for epoch ∈ 1:epochs
+		Flux.train!(loss, Flux.params(model), data, Adam(η))
+		a = accuracy(model(x), y)
+		l = loss(x, y)
+		@info "epoch $(epoch); acc=$(round(a, digits=2))"
+		loss_hist[epoch] = l
+		accuracy_hist[epoch] = a
+	end
+end
+
+# Accuracy after training
+accuracy(model(x), y)
+
+
+let
+	p1 = plot(loss_hist, title="Loss", label=:none, xlabel="Training step × 1000")
+	p2 = plot(accuracy_hist, title="Accuracy", label=:none)
+	p3 = confusion_plot(Flux.onecold(model(x)), Flux.onecold(y))
+	plot(p1, p2, p3, layout=(2,2), size=(800, 700))
+end
+
+confusion_plot(ŷ, y; kwargs...) = heatmap(sort(unique(y)), sort(unique(y)), 
+	[sum((ŷ .== i) .& (y .== j)) for i=unique(y), j=unique(y)],
+	xlabel="y", ylabel="ŷ", size=(400, 300), colorbartitle="Count"; kwargs...)
--- content/Code/EmmaNNCode.jl
+++ content/Code/EmmaNNCode.jl
@@ -1,9 +1,13 @@
+import Pkg; 
+Pkg.add("GLM")
+
 using DataFrames
 using Plots
 using CSV
 using Random
 using Flux
 using Flux: train!
+using GLM
 
 
 data = CSV.read("C:/Users/emmag/OneDrive/EMMA_user/Docs/Academics/UIUC/2022-2023/CEE 492 Data Science/Project/TreeData/Data/TS3_Raw_tree_data.csv", DataFrame)
@@ -11,9 +15,12 @@ df = select(data,["DBH", "TreeHt", "Age", "CrnBase"])
 dff = filter(x -> (x.DBH != -1) && (x.Age != -1) && (x.TreeHt != -1) && (x.CrnBase != -1), df)
 dff = shuffle(dff)
 
+#########################################################################################
+# Method 1: one input variable one output variable
+
 # Change x to test prediction accuracy of different variables of interest (i.e., DBH, tree height)
-x = Vector(dff.CrnBase)
-y = Vector(dff.Age)
+x = Vector(dff.DBH)
+y = Vector(dff.TreeHt)
 
 # GOAL: TRY USING MULTIPLE INDEPENDENT INPUT VARIABLES IN NEURAL NET
 
@@ -40,8 +47,6 @@ y_train, y_test = rand(length(x_train)), y[6277:end] # set training y data as du
 #plot(x_train, y_train, title="Simple linear regression model", label="Train data", seriestype = :scatter)
 #plot!(x_test, y_test, label="Test data", seriestype = :scatter)
 
-
-# METHOD 1: Simple Linear Regression NN Model
 reduce(hcat, x_train)
 
 function reshape_data(X, y) # Flux expects the data as a 1x#elements array. Use the hcat (horizontal concatenation) and the reduce (high order function) functions to obtain the correct shape.
@@ -56,6 +61,7 @@ x_test, y_test = reshape_data(x_test, y_test)
 
 # Create a simple linear regression model m(x) = W*x + b. 
 model = Dense(size(x_train,1), 1, σ) # Implements the function σ(Wx+b) where W and b are the weights and biases. σ is an activation function
+println(model)
 
 # Before we move on, we need to collect all of the parameters so we can access and update them during the training steps. We do this with the params function.
 ps = Flux.params(model)
@@ -90,67 +96,128 @@ pred_1 = model(x_test)
 println(pred_1)
 
 RSME = (round((sum(((y_test) .- model(x_test)).^2) / length((y_test)))^0.5, digits=1) )
-#~ RSME = 38... because of model structure or poor correlation between variables?
+#~ RSME = 38 for CrnBase --> Age... because of model structure or poor correlation between variables?
+#~ RSME = 13.2 for DBH --> TreeHt... because of model structure or poor correlation between variables?
 
-plot(x_train, y_train, title="Simple linear regression model using Flux", label="Train data", seriestype = :scatter)
+Accuracy = sum(abs.(pred_1 .- y_test) ./ y_test)/length(y_test)
+#~ Acc = 0.88 for DBH --> TreeHt
+
+#Rsq = r2(model)
+
+#=plot(x_train, y_train, title="Simple linear regression model using Flux", label="Train data", seriestype = :scatter)
 plot!(x_test, y_test, label="Test data", seriestype = :scatter)
 plot!(x_test, pred_0, label="Initial predictions")
 plot!(x_test, pred_1, label="Final predictions")
+=#
 
 
 
+############################################################################################
+# RUN SEPARATELY, REFRESH WORKSPACE
+# Method 2: multiple input variables, one output variable
+
+data = CSV.read("C:/Users/emmag/OneDrive/EMMA_user/Docs/Academics/UIUC/2022-2023/CEE 492 Data Science/Project/TreeData/Data/TS3_Raw_tree_data.csv", DataFrame)
+df = select(data,["DBH", "TreeHt", "Age", "CrnBase", "CrnHt", "AvgCdia", "Leaf"])
+dff = filter(x -> (x.DBH != -1) && (x.Age != -1) && (x.TreeHt != -1) && (x.CrnBase != -1) && (x.CrnHt != -1) && (x.AvgCdia != -1) && (x.Leaf != -1), df)
+dff = shuffle(dff)
 
 
-#= By hand methods, didn't work
+x = Matrix(select(data,["TreeHt", "Age", "CrnBase", "CrnHt", "AvgCdia", "Leaf"]))
+y = Vector(dff.DBH)
+typeof(x)
 
-#Error Metric
-function mse(ŷ::Vector, y::Vector)
-    mean((ŷ .- y).^2)
-    end
+# GOAL: TRY USING MULTIPLE INDEPENDENT INPUT VARIABLES IN NEURAL NET
 
-    function relu(x)
-        max(zero(x),x)
-        end
+#= function partitionTrainTest(data, at = 0.5)
+    n = nrow(data)
+    idx = shuffle(1:n)
+    train_idx = view(idx, 1:floor(Int, at*n))
+    test_idx = view(idx, (floor(Int, at*n)+1):n)
+    data[train_idx,:], data[test_idx,:]
+end
 
-        function threelayer_logistic(x, p::Vector) 
-        w1, b1, w2, b2, w3, b3 = p
-        output1 = relu.(w1*x .+ b1)
-        output2 = relu.(w2*output1 .+ b2)
-        output3 = w3*output2 .+ b3
-        end
-
-        function train!(modelf, errf, p, x, y::Vector,η, nsteps::Int)
-            function er(p)
-            errf(modelf(x,p)[:],y)
-            end
-            for i in 1:nsteps
-            p -= er'(p) * η
-            end
-            p
-            end
-
-            begin
-                inputsize = length(x_train) # number of rows!!!
-                hiddensize = 1
-                nlabels = 1 # This is the number of dependent variables we want to predict.
-                # Initialize weights and biases with random values.
-                w1 = rand(hiddensize, inputsize) .- 0.5
-                b1 = rand(hiddensize) .- 0.5
-                w2 = rand(hiddensize, hiddensize) .- 0.5
-                b2 = rand(hiddensize) .- 0.5
-                w3 = rand(nlabels, hiddensize) .- 0.5
-                b3 = rand(nlabels) .- 0.5
-                p = [w1, b1, w2, b2, w3, b3] # Create our vector of parameter objects.
-                η = .001 #Learning rate is between 0 to 1
-                nsteps = 10000
-                p = train!(threelayer_logistic, mse, p, x_train, y_train, η, nsteps)
-            end
-
-                function predict_transmission(x::Vector)
-                    input = threelayer_logistic(x,p)
-                    return vec(input)
-                end
-
-                RSME = (round((sum((Vector(y_test) .- predict_transmission(x_test)).^2) / length(Vector(y_test)))^0.5, digits=1) )
-
+using RDatasets
+# dff = dataset("datasets", "iris")
+x_train,x_test = partitionTrainTest(dff.DBH, 0.5) # 50% train
+y_train,y_test = partitionTrainTest(dff.Age, 0.5) # 50% train
  =#
+
+x_train, x_test = x[1:5624,:], x[5625:end,:] # 50% of data as training, 50% as test, start with just one input
+y_train, y_test = rand(size(x_train,1)), y[5625:end] # set training y data as dummy, but testing y data as actual
+
+#println(x_train)
+#println(y_train)
+
+#plot(x_train, y_train, title="Simple linear regression model", label="Train data", seriestype = :scatter)
+#plot!(x_test, y_test, label="Test data", seriestype = :scatter)
+
+reduce(hcat, x_train)
+
+function reshape_data(X, y) # Flux expects the data as a 1x#elements array. Use the hcat (horizontal concatenation) and the reduce (high order function) functions to obtain the correct shape.
+    X = reduce(hcat, X)
+    y = reduce(hcat, y)
+    return X, y    
+end 
+
+x_train, y_train = reshape_data(x_train, y_train)
+x_test, y_test = reshape_data(x_test, y_test)
+
+
+# Create a simple linear regression model m(x) = W*x + b. 
+model = Dense(size(x_train,1), 1, σ) # Implements the function σ(Wx+b) where W and b are the weights and biases. σ is an activation function
+println(model)
+
+# Before we move on, we need to collect all of the parameters so we can access and update them during the training steps. We do this with the params function.
+ps = Flux.params(model)
+
+# Measure the predictions that our model makes so we can determine how good they are.
+loss(x, y) = Flux.Losses.mse(model(x), y)
+
+# Set the optimisation routine (optimiser) that we’ll use to train our model. This optimiser will optimise the loss function by updating parameters in our model.
+opt = Descent()
+
+# Before we train our model, we compute the predictions and the current loss to compare with the final results
+pred_0 = model(x_train)
+println(pred_0)
+
+loss_0 = loss(model(x_train), y_train)
+println(loss_0)
+
+
+# Train the Model
+data = [(x_train, y_train)]
+
+# Execute the train steps (epochs) until the W and b parameters minimize the loss function.
+n_epochs = 12
+
+for epoch in 1:n_epochs
+    train!(loss, ps, data, opt)
+    println(loss(model(x_test), y_test))
+end
+
+
+pred_1 = model(x_test)
+println(pred_1)
+
+RSME = (round((sum(((y_test) .- model(x_test)).^2) / length((y_test)))^0.5, digits=1) )
+#~ RSME = 38 for CrnBase --> Age... because of model structure or poor correlation between variables?
+#~ RSME = 13.2 for DBH --> TreeHt... because of model structure or poor correlation between variables?
+
+Accuracy = sum(abs.(pred_1 .- y_test) ./ y_test)/length(y_test)
+#~ Acc = 0.88 for DBH --> TreeHt
+
+#Rsq = r2(model)
+
+#=plot(x_train, y_train, title="Simple linear regression model using Flux", label="Train data", seriestype = :scatter)
+plot!(x_test, y_test, label="Test data", seriestype = :scatter)
+plot!(x_test, pred_0, label="Initial predictions")
+plot!(x_test, pred_1, label="Final predictions")
+=#
+
+
+
+
+
+############################################################################################
+# RUN SEPARATELY, REFRESH WORKSPACE
+# Method 3: multiple input variables, one output variable: logistic regression and softmax (classification)
